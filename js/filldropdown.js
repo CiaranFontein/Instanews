ca8d4c00158d2bf0a83c0da1fd07d37e65dc0319
Ciaran Fontein <ciaranfontein@gmail.com>
2019-07-25 08:50:12 -0700

    Gets images and📝 abstracts from ny💯 times and puts into 🚼grid

---
+++ js/filldropdown.js
@@ -0,0 +1,121 @@
+$(document).ready(function() {
+  let sectionTitle = "null";
+  let results = [];
+  let sections = [
+    "arts",
+    "automobiles",
+    "books",
+    "business",
+    "fashion",
+    "food",
+    "health",
+    "home",
+    "insider",
+    "magazine",
+    "movies",
+    "national",
+    "nyregion",
+    "obituaries",
+    "opinion",
+    "politics",
+    "realestate",
+    "science",
+    "sports",
+    "sundayreview",
+    "technology",
+    "theater",
+    "tmagazine",
+    "travel",
+    "upshot",
+    "world"
+  ];
+
+  for (var i = 0; i < sections.length; i++) {
+    $(".dropdown").append("<option>" + sections[i] + "</option>");
+  }
+
+  $(".dropdown").on("change", function() {
+    const selected = $(this).val();
+    sectionTitle = selected;
+    let sectionURL =
+      "https://api.nytimes.com/svc/topstories/v2/" +
+      sectionTitle +
+      ".json?api-key=9A0091nJwReeB9Uk3aD0VFbax9hwrv6p";
+    loadArticles(sectionURL);
+  });
+
+  function loadArticles(sectionURL) {
+    $.ajax({
+      method: "get",
+      url: sectionURL
+    })
+      .done(function(data) {
+        results = data.results;
+
+        for (var i = 0; i < results.length; i++) {
+          article = results[i];
+          $(".content-grid").append(
+            //'<a href="' +
+            article.url +
+              '"><div class="content-cell" id="content-cell-' +
+              i +
+              '"><div class="text-box">' +
+              article.abstract +
+              "</div></div>"
+            //</a>"
+          );
+
+          //Make changes to specific grid cell
+
+          if (article.multimedia.length > 4) {
+            $("#content-cell-" + i).css(
+              "background-image",
+              'url("' + article.multimedia[4].url
+            );
+          }
+          $("#content-cell-" + i).on("click", function() {
+            readOutLoud(article.abstract);
+          });
+          unirest
+            .get(
+              "https://microsoft-azure-translation-v1.p.rapidapi.com/Speak?text=Hello%2C+world!&language=en"
+            )
+            .header(
+              "X-RapidAPI-Host",
+              "microsoft-azure-translation-v1.p.rapidapi.com"
+            )
+            .header(
+              "X-RapidAPI-Key",
+              "d42864f054msh6fdf3d04acfdf39p1bbdd3jsnef6c6dfd558e"
+            )
+            .end(function(result) {
+              console.log(result.status, result.headers, result.body);
+            });
+        }
+        $(".content-cell").css({
+          "background-size": "cover",
+          height: "300px",
+          "vertical-align": "bottom",
+          display: "flex",
+          "align-items": "flex-end",
+          "justify-content": "flex-end"
+        });
+        console.log(results);
+      })
+      .fail(function() {
+        console.log("fail");
+      })
+      .always();
+  }
+
+  function readOutLoud(readThisString) {
+    let ttsURL =
+      "https://microsoft-azure-translation-v1.p.rapidapi.com/Speak?text=" +
+      readThisString +
+      "&language=en";
+    $.ajax({
+      method: "get",
+      url: ttsURL
+    });
+  }
+});
